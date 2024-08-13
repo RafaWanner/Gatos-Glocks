a1265fcf571ab1d634b70cc8053b2df1aa39343f
flepes <felgom@alunos.utfpr.edu.br>
2024-08-12 23:30:23 -0300

    add fire rate and life

--- gun.gd
+++ gun.gd
@@ -1,18 +1,33 @@
 extends Area2D
 
+var shooting = false  # Variável para controlar o estado de disparo
+@onready var fireRate := $FireRateTimer
+var can_shoot = true
+var fire_rate = 0.5
 
 func _physics_process(delta):
-	# Obter a posição do mouse
+	
+	if Input.is_action_pressed("shoot") and can_shoot:
+		shoot()
+		can_shoot = false
+		await(get_tree().create_timer(fire_rate).timeout)
+		can_shoot = true
+	
 	var mouse_position = get_global_mouse_position()
 	look_at(mouse_position)
 	
-	if Input.is_action_just_pressed("shoot"):
-		shoot()
-		
+
+
+
 func shoot():
 	const BULLET = preload("res://bullet.tscn")
 	var new_bullet = BULLET.instantiate()
+	var shooting_point = $ShootingPoint  # ou get_node("ShootingPoint") se preferir
+	
 	new_bullet.global_position = %ShootingPoint.global_position
 	new_bullet.global_rotation = %ShootingPoint.global_rotation
-	%ShootingPoint.add_child(new_bullet)
+	%ShootingPoint.add_child(new_bullet) # Adiciona a bala à cena atual
+	fireRate.start()
 
+
+	
--- player.gd
+++ player.gd
@@ -1,28 +1,33 @@
 extends CharacterBody2D
 
-
 signal health_deplated
-var health = 100.0
 
+var health = 5
+var invincibility_duration = 1.0  # Tempo de invencibilidade em segundos
+var is_invincible = false
 
 func _physics_process(delta):
-	var direction = Input.get_vector("move_left", "move_right",
-"move_up", "move_down")
-
+	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	
+	get_node("/root/Game/HealthLabel").text = str(health)
 	velocity = direction * 600
 	move_and_slide()
 	
 	if velocity.length() > 0.0:
 		get_node("HappyBoo").play_walk_animation()
-	else: 
+	else:
 		get_node("HappyBoo").play_idle_animation()
 
-	const DAMAGE_RATE = 20.0
-	var overlappping_mobs = %HurtBox.get_overlapping_bodies()
-	if overlappping_mobs.size() > 0:
-		health -= DAMAGE_RATE * overlappping_mobs.size() * delta
-		%ProgressBar.value = health
-		if health <= 0.0:
-			health_deplated.emit()			
-	
-		
+	const DAMAGE_RATE = 1
+	var overlapping_mobs = get_node("HurtBox").get_overlapping_bodies()
+	if overlapping_mobs.size() > 0 and not is_invincible:
+		health -= DAMAGE_RATE
+		get_node("/root/Game/HealthLabel").text = str(health)
+		if health <= 0:
+			health_deplated.emit()
+		_start_invincibility()
+
+func _start_invincibility():
+	is_invincible = true
+	await(get_tree().create_timer(invincibility_duration).timeout)
+	is_invincible = false
