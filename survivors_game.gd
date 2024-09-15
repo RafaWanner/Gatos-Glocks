3befc0ff1cd6d36a8dd5ad8decd0364ac50ed090
flepes <felgom@alunos.utfpr.edu.br>
2024-09-15 18:12:12 -0300

    some changes

--- survivors_game.gd
+++ survivors_game.gd
@@ -7,6 +7,9 @@ extends Node2D
 var paused = false
 var is_lvlup_menu = false
 
+var acceleration_rate = 0.98  # Quanto o tempo do timer deve reduzir
+var min_timer_interval = 1.0  # Tempo mínimo de intervalo para o timer
+
 func _process(delta):
 	if Input.is_action_just_pressed("pause"):
 		_pause_menu()
@@ -51,27 +54,40 @@ func spawn_mob2():
 	add_child(new_mob)
 	
 func spawn_mob3():
-	var  new_mob = preload("res://omago.tscn").instantiate()
+	var new_mob = preload("res://omago.tscn").instantiate()
 	%PathFollow2D.progress_ratio = randf()
 	new_mob.global_position = %PathFollow2D.global_position
 	add_child(new_mob)
 
 func _on_timer_timeout():
 	spawn_mob()
+	accelerate_timers()
 
 func _on_timer_2_timeout():
 	spawn_mob2()
-	
+	accelerate_timers()
+
 func _on_timer_3_timeout():
 	spawn_mob3()
+	accelerate_timers()
 
+# Funções para restartar os timers
 func _on_timer_2_timer_timeout():
 	$Timer2.start()
-
-
-
 
 func _on_timer_3_timer_timeout():
 	$Timer3.start()
 
+# Função para acelerar os timers
+func accelerate_timers():
+	# Timer 1
+	if $Timer.wait_time > min_timer_interval:
+		$Timer.wait_time *= acceleration_rate
 
+	# Timer 2
+	if $Timer2.wait_time > min_timer_interval:
+		$Timer2.wait_time *= acceleration_rate
+
+	# Timer 3
+	if $Timer3.wait_time > min_timer_interval:
+		$Timer3.wait_time *= acceleration_rate
